9620782c758eb89cc46bc5dd2710b0206b5a9bd2
Louis Demange <louis.demange.m@gmail.com>
2020-02-29 00:09:58 +0100

    improve script

--- fetch.sh
+++ fetch.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+cd /usr/share/backgrounds/earth
 json=`curl -s https://epic.gsfc.nasa.gov/api/natural`
 date=`echo "$json" | jq '.[-1].date'`
 img=`echo "$json" | jq '.[-1].image' | tr -d '"'`
 
-if [ -f "$img.png" ]
+if [ ! -f "$img.png" -o ! -f "wallpaper.png" ]
 then
-	exit
+	rm -f epic_*.png
+	curl -s "https://epic.gsfc.nasa.gov/archive/natural/${date:1:4}/${date:6:2}/${date:9:2}/png/$img.png" > "$img.png"
+	convert $img.png -resize 1080x1080 -gravity center -background black -extent 1920x1080 wallpaper.png
 fi
 
-rm -f epic_*.png
-curl -s "https://epic.gsfc.nasa.gov/archive/natural/${date:1:4}/${date:6:2}/${date:9:2}/png/$img.png" > $img.png
-convert $img.png -resize 1080x1080 -gravity center -background black -extent 1920x1080 wallpaper.png
 gsettings set org.gnome.desktop.background picture-options 'scaled'
+gsettings set org.gnome.desktop.background picture-uri '' # to force update
 gsettings set org.gnome.desktop.background picture-uri file://`pwd`/wallpaper.png
